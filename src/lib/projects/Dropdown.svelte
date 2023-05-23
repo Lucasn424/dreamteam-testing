cf4964b0f8abc4929cb7dbfec1ca911d66eb8dce
Chinene <73177313+Chinene-C@users.noreply.github.com>
2023-05-23 09:28:21 +0100

    added needed html

--- src/lib/projects/Dropdown.svelte
+++ src/lib/projects/Dropdown.svelte
@@ -17,12 +17,22 @@
     .dropdown-content.open {
       display: block;
     }
+  
+    .dropdown-content h3 {
+      font-size: 1.125rem; /* equivalent to text-lg */
+    }
+  
+    .dropdown-content p {
+      text-align: justify;
+      font-size: 0.875rem; /* equivalent to text-sm */
+    }
   </style>
   
   <button on:click={toggleDropdown}>{buttonText}</button>
   
   {#if isOpen}
     <div class="dropdown-content open">
-      <p>{dropdownContent}</p>
+      <h3 class="text-lg">{dropdownContent.title}</h3>
+      <p class="text-justify text-sm">{dropdownContent.text}</p>
     </div>
   {/if}
