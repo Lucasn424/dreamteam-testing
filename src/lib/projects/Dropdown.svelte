a2bfa90f0e0139c2a9679dc7568aca485c60881d
Chinene <73177313+Chinene-C@users.noreply.github.com>
2023-05-23 09:27:45 +0100

    adapted so selected and deselected when wanted

---
+++ src/lib/projects/Dropdown.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let buttonText = 'Toggle Dropdown';
+    export let dropdownContent = '';
+  
+    let isOpen = false;
+  
+    function toggleDropdown() {
+      isOpen = !isOpen;
+    }
+  </script>
+  
+  <style>
+    .dropdown-content {
+      display: none;
+    }
+  
+    .dropdown-content.open {
+      display: block;
+    }
+  </style>
+  
+  <button on:click={toggleDropdown}>{buttonText}</button>
+  
+  {#if isOpen}
+    <div class="dropdown-content open">
+      <p>{dropdownContent}</p>
+    </div>
+  {/if}
