4a61fc03088823332985099ad5a7980a9ad134e5
Chinene <73177313+Chinene-C@users.noreply.github.com>
2023-05-23 08:31:31 +0100

    made caption overlay component

---
+++ src/lib/projects/CaptionedImage.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let src;
+  export let alt;
+  export let caption;
+  let isHovered = false;
+
+  function handleFocus() {
+    isHovered = true;
+  }
+
+  function handleBlur() {
+    isHovered = false;
+  }
+</script>
+
+<style>
+  .container {
+    position: relative;
+    display: inline-block;
+  }
+
+  .caption {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+    padding: 8px;
+    text-align: center;
+    font-size: 14px;
+    opacity: 0;
+    pointer-events: none;
+    transition: opacity 0.2s;
+  }
+
+  .show-caption {
+    opacity: 1;
+  }
+</style>
+
+<div
+  class="container"
+  on:mouseover={handleFocus}
+  on:mouseout={handleBlur}
+  on:focus={handleFocus}
+  on:blur={handleBlur}
+  tabindex="0"
+>
+  <img {src} {alt} />
+  <div class="caption" class:show-caption={isHovered}> {caption} </div>
+</div>
